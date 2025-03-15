46dc70313b93b55405dcbf55ef6afd326363a26f
Qurat-AI <malikmantsha08@gmail.com>
2025-03-14 23:57:59 -0700

    Al junaid python task 2

---
+++ books.py
@@ -0,0 +1,7 @@
+Books = {}
+
+Books["986-4-65-565546"]=("English", "charle",65.87,{"education","thriller"})
+Books["986-4-65-565546"]=("urdu", "charle",65.87,{"education","thriller"})
+Books["986-4-65-565546"]=("programming", "charle",65.87,{"j.don","thriller"})
+
+print(Books)
